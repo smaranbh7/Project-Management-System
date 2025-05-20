61ac7ba2c7e0e4c4adfae19a7ca60b392131f046
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-05-20 01:22:27 -0400

    CustomFilterchain and JWT Filter created

---
+++ src/main/java/com/smaran/projectmanagementsystem/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.smaran.projectmanagementsystem.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="dasfasdfdsafdsaghrtrwgcvszvc";
+    public static final String JWT_HEADER="Authorization";
+}
