efe99828e50191b29b3b7e4ba0ccdb10de2cc8aa
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-05-21 15:11:26 -0400

    signup controller

--- src/main/java/com/smaran/projectmanagementsystem/controller/AuthController.java
+++ src/main/java/com/smaran/projectmanagementsystem/controller/AuthController.java
@@ -1,10 +1,21 @@
 package com.smaran.projectmanagementsystem.controller;
 
 
+import com.smaran.projectmanagementsystem.config.JwtProvider;
+import com.smaran.projectmanagementsystem.model.User;
 import com.smaran.projectmanagementsystem.repo.userRepo;
+import com.smaran.projectmanagementsystem.response.AuthResponse;
 import com.smaran.projectmanagementsystem.service.CustomUserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +29,28 @@ public class AuthController {
     @Autowired
     private CustomUserDetailsImpl customUserDetails;
 
+    @PostMapping("/signup")
+    public ResponseEntity<User> createUserHandler(@RequestBody User user) throws Exception {
+        User isUserExist = userRepo.findByEmail(user.getEmail());
+        if(isUserExist!=null){
+            throw new Exception("email Already exist!");
+        }
+        User createdUser= new User();
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFullName(user.getFullName());
+        User savedUser= userRepo.save(createdUser);
 
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = JwtProvider.generateToken(authentication);
+
+        AuthResponse res= new AuthResponse();
+        res.setMessage("Signup Success");
+        res.setJwt(jwt);
+
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
 
 }
