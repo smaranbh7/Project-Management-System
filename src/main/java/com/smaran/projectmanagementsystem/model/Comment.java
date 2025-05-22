99320d0114b60f4741acf6d9c0412b1db0f70a94
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-05-22 15:38:51 -0400

    All Required Models Created

---
+++ src/main/java/com/smaran/projectmanagementsystem/model/Comment.java
@@ -0,0 +1,28 @@
+package com.smaran.projectmanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    private LocalDate createdDateTime;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Issue issue;
+}
---
+++ src/main/java/com/smaran/projectmanagementsystem/model/Message.java
@@ -0,0 +1,28 @@
+package com.smaran.projectmanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Chat chat;
+
+    @ManyToOne
+    private User sender;
+}
