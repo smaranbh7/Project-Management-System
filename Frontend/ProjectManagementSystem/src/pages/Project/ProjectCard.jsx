fb841d56698b20c4c51c928b230e75be309e9880
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-05-29 14:56:12 -0400

    Frontend - Card section done

--- Frontend/ProjectManagementSystem/src/pages/Project/ProjectCard.jsx
+++ Frontend/ProjectManagementSystem/src/pages/Project/ProjectCard.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import { Button } from "../../components/ui/button";
 import { Card } from "../../components/ui/card";
-import { DotFilledIcon } from "@radix-ui/react-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../components/ui/dropdown-menu";
+import { Badge } from "../../components/ui/badge"
+import { DotFilledIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
 
 function ProjectCard() {
   return (
     <Card className="p-5 w-full lg:max-w-3xl">
-      <div className="space-y-5"></div>
-      <div className="space-y-2"></div>
-      <div className="flex justify-between">
-        <div className="flex items-center gap-5">
-          <h1 className="cursor-pointer font-bold text-lg">
-            Create Ecommerce Project
-          </h1>
-          <DotFilledIcon />
-          <p className="text-sm text-gray-5=400">FullStack</p>
+      <div className="space-y-5">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-5">
+              <h1 className="cursor-pointer font-bold text-lg">
+                Create Ecommerce Project
+              </h1>
+              <DotFilledIcon />
+              <p className="text-sm text-gray-5=400">FullStack</p>
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button className="rounded-full" variant="ghost" size="icon">
+                    <DotsVerticalIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Update</DropdownMenuItem>
+                  <DropdownMenuItem>Delete</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+          <p className="text-gray-500 text-sm">
+            Lorem ipsum dolor sit amet cam atque voluptatem incidunt.
+            </p>
+        </div>
+        <div className="flex flex-wrap gap-2 items-center">
+            {[1,1,1,1].map((item)=><Badge key={item} variant="outline">{"frontend"}</Badge>)}
+
         </div>
       </div>
     </Card>
